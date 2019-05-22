2c6349995e0782e4ec04de6e81de94238b565d9b
William Azevedo <willianray@gmail.com>
2019-05-22 09:13:11 -0300

    Removido código desnecessário.

--- background_script.js
+++
@@ -1,7 +0,0 @@
-// chrome.tabs.getAllInWindow(tabs => {
-//   const bitmexTab = tabs.filter(tab => tab.url.includes('bitmex.com/app/trade/'))[0];
-//   chrome.tabs.executeScript(bitmexTab.id, {
-//     file: 'execute_script.js'
-//   });
-// });
-console.log('');
